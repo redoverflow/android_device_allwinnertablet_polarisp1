d000414b0e17160d6002a101fc9a4845550729f9
redoverflow <punctuwuation@gmail.com>
2022-10-09 17:01:46 +0300

    a bit of fixing

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/aliexpress2/polaris_p1/AndroidBoardVendor.mk
+-include vendor/allwinnertablet/polarisp1/AndroidBoardVendor.mk
